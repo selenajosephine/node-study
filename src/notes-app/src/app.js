9ef55e3c830e6fbb35f441aecf19f69279b90ca4
Selena Josephine Ponmani D <selenajosephine@gmail.com>
2020-01-13 23:28:39 +0530

    importing modules in node

--- src/notes-app/src/app.js
+++ src/notes-app/src/app.js
@@ -1,11 +1,9 @@
-const fs = require('fs');
+var notes = require('./notes');
+console.log(notes.getNotes());
 
-// fs.writeFileSync('notes.txt','This file was created by nodejs written by Selena');
-// two ways to append data to a file
-// 1. Read the file, append data and write the file (overwrite)
-var textFromFile = fs.readFileSync('notes.txt');
-var textToFile = textFromFile + "and this is text appended by code";
-fs.writeFileSync('notes.txt', textToFile);
-
-// 2. append text to file
-fs.appendFileSync('notes.txt', "and this is text appended by code");
+/** 
+ * Reference code for importing new modules
+ */
+// var utils = require('./test1.js');
+// var sum = utils.add(2, 3);
+// console.log(sum);
