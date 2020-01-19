3742bbbe23d38e0e283ed9c43c43411a3d6db590
Selena Josephine Ponmani D <selenajosephine@gmail.com>
2020-01-19 17:57:04 +0530

    creating cli commands

--- notes-app/src/app.js
+++ notes-app/src/app.js
@@ -1,19 +1,22 @@
-const chalk = require('chalk');
-var yargs = require('yargs').argv;
-var notes = require('./notes');
-var command = process.argv[2].toUpperCase();
-console.log(process.argv);
-console.log('yargs', yargs);
-console.log(yargs.title);
-
-switch (command) {
-    case "ADD":
-        console.log("inside add");
-        break;
-    case "REMOVE":
-        console.log("inside remove");
-        break;
-    default:
-        console.log("Try again");
-        break;
-}
+#!/usr/bin/env node
+const argv = require('yargs')
+    // register command - add to CLI 
+    // command name, command description, handler function
+    .command('add', 'add a new note', function () {
+        console.log('adding a new note');
+    })
+    // register command - remove to CLI 
+    .command('remove', 'delete a note', function () {
+        console.log('removing the note');
+    })
+    // register command - list to CLI 
+    .command('list', 'list the notes', function () {
+        console.log('listing the notes');
+    })
+    // register command - read to CLI 
+    .command('read', 'read a note', function () {
+        console.log('reading a note');
+    })
+    .help('h')
+    .alias('h', 'help')
+    .argv
