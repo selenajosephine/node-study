9d219de799b3dbe2f9be8f4e17c786ac27ecbf2f
Selena Josephine Ponmani D <selenajosephine@gmail.com>
2020-01-19 18:49:25 +0530

    cli properties added

--- notes-app/src/app.js
+++ notes-app/src/app.js
@@ -1,22 +1,51 @@
 #!/usr/bin/env node
 const argv = require('yargs')
     // register command - add to CLI 
-    // command name, command description, handler function
-    .command('add', 'add a new note', function () {
-        console.log('adding a new note');
+    // command name, command description, builder properties, handler function
+    .command('add', 'Add a new note', {
+        // property name
+        title: {
+            // title description
+            describe: 'note title',
+            // required true
+            demandOption: true,
+            // data type string
+            type: 'string'
+        },
+        body: {
+            describe: 'note content',
+            demandOption: true,
+            type: 'string'
+        }
+    }, (argv) => {
+        console.log("title : " + argv.title);
+        console.log("note content: " + argv.body);
     })
     // register command - remove to CLI 
-    .command('remove', 'delete a note', function () {
-        console.log('removing the note');
+    .command('remove', 'Delete a note', {
+        title: {
+            describe: 'note to be deleted',
+            demandOption: true,
+            type: 'string'
+        }
+    }, (argv) => {
+        console.log(argv.title);
     })
     // register command - list to CLI 
-    .command('list', 'list the notes', function () {
+    .command('list', 'List the notes', () => {
         console.log('listing the notes');
     })
     // register command - read to CLI 
-    .command('read', 'read a note', function () {
-        console.log('reading a note');
+    .command('read', 'Read a note', {
+        title: {
+            describe: 'read note',
+            demandOption: true,
+            type: 'string'
+        }
+    }, (argv) => {
+        console.log('reading note with title' + argv.title)
     })
     .help('h')
     .alias('h', 'help')
     .argv
+
