38ce85c060b1b5ffa1de44420077a9b1c2c5ff15
Selena Josephine Ponmani D <selenajosephine@gmail.com>
2020-02-08 21:52:38 +0530

    retreiving latitude and longitude changes

--- weather-app/src/app.js
+++ weather-app/src/app.js
@@ -3,6 +3,17 @@ const url = 'https://api.darksky.net/forecast/399f47b1909ba2f6042da8815e1397ed/3
 
 request({ url, json: true }, (err, response) => {
     if (err) console.log(err);
-    console.log(response.body.currently.precipProbability);
-    console.log('it is currently ' + response.body.currently.temperature + ' degrees, and there is '+ (response.body.currently.precipProbability*100)+ '% chances of rain');
+    //  console.log(response.body);
+    console.log(response.body.daily.data[0].summary +
+        ' It is currently ' + response.body.currently.temperature + ' degrees, and there is ' + (response.body.currently.precipProbability * 100) + '% chances of rain');
 })
+
+const locationUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/chester.json?proximity=-74.70850,40.78375&access_token=pk.eyJ1Ijoic2VsZW5ham9zZXBoaW5lIiwiYSI6ImNrNjU3NzN6cjA0eWszZGx2YjRudTlqcDUifQ.OQobJ_TStm-4k7V_cqT8AQ"
+request({ url: locationUrl, json: true }, (err, response) => {
+    if (err) console.log(err);
+    const longitude = response.body.features[0].geometry.coordinates[0];
+    const latitude = response.body.features[0].geometry.coordinates[1];
+    console.log('latitude', latitude, 'longitude', longitude);
+})
+
+// user will give address => Lat/Long => Weather
