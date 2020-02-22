ca8fd0a33791189af18b2c54950fff0277a11aa4
Selena Josephine Ponmani D <selenajosephine@gmail.com>
2020-02-22 22:16:45 +0530

    weather app changes

--- weather-app/src/app.js
+++ weather-app/src/app.js
@@ -2,18 +2,25 @@ const request = require('request');
 const url = 'https://api.darksky.net/forecast/399f47b1909ba2f6042da8815e1397ed/37.8267,-122.4233';
 
 request({ url, json: true }, (err, response) => {
-    if (err) console.log(err);
+    if (err) return err;
     //  console.log(response.body);
     console.log(response.body.daily.data[0].summary +
         ' It is currently ' + response.body.currently.temperature + ' degrees, and there is ' + (response.body.currently.precipProbability * 100) + '% chances of rain');
 })
 
-const locationUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/chester.json?proximity=-74.70850,40.78375&access_token=pk.eyJ1Ijoic2VsZW5ham9zZXBoaW5lIiwiYSI6ImNrNjU3NzN6cjA0eWszZGx2YjRudTlqcDUifQ.OQobJ_TStm-4k7V_cqT8AQ"
+const locationUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoic2VsZW5ham9zZXBoaW5lIiwiYSI6ImNrNjU3NzN6cjA0eWszZGx2YjRudTlqcDUifQ.OQobJ_TStm-4k7V_cqT8AQ"
 request({ url: locationUrl, json: true }, (err, response) => {
-    if (err) console.log(err);
-    const longitude = response.body.features[0].geometry.coordinates[0];
-    const latitude = response.body.features[0].geometry.coordinates[1];
-    console.log('latitude', latitude, 'longitude', longitude);
+    if (err) {
+        console.log("unable to connect to the server");
+    }
+    else if (response.body.features && response.body.features.length == 0) {
+        console.log("Unable to find the location");
+    }
+    else{
+        const longitude = response.body.features[0].geometry.coordinates[0];
+        const latitude = response.body.features[0].geometry.coordinates[1];
+        console.log('latitude', latitude, 'longitude', longitude);
+    }
 })
 
 // user will give address => Lat/Long => Weather
