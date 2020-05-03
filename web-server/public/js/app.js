0aaddc3d1ba0375aef82d3d359b554167f1f23cb
unknown <selenajosephine@gmail.com>
2020-05-03 19:43:24 +0530

    final weather app commit

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,16 +1,23 @@
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
 weatherForm.addEventListener("submit", (e) => {
     e.preventDefault();
     const location = search.value;
+    messageTwo.textContent = 'Loading...';
+    messageOne.textContent = '';
     fetch('http://localhost:3000/weather?address=' + location).then(res => {
         res.json().then((data) => {
             if (data.error) {
-                console.log(data.error)
+                messageTwo.textContent = data.error;
+                messageOne.textContent = '';
             }
             else {
-                console.log(data.location, data.forecast)
+                messageOne.textContent = data.location;
+                messageTwo.textContent = data.forecast;
             }
         })
     }).catch(err => {
