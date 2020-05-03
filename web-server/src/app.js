08284c9620c750fb21e73cdc588cf78036211935
unknown <selenajosephine@gmail.com>
2020-05-03 19:09:30 +0530

    read location from ui changes

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,6 +2,9 @@ const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
 
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
+
 const app = express();
 
 // define paths for express config
@@ -43,12 +46,42 @@ app.get('/help/*', (req, res) => {
     res.render("pagenotfound", {
         title: "404",
         name: 'Selena',
-        errorMessage:'Help article not found'
+        errorMessage: 'Help article not found'
     });
 })
 
 app.get("/weather", (req, res) => {
-    res.send({ location: "Philadelphia", forecast: "mostly sunny" })
+    if (!req.query.address) {
+        return res.send({
+            error: 'Address is a required field'
+        });
+    }
+    geocode(req.query.address, (error, { longitude, latitude, location } = {}) => {
+        if (error) {
+            return res.send({ error });
+        }
+        forecast(longitude, latitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error });
+            }
+            return res.send({
+                location,
+                forecast: forecastData,
+                address: req.query.address
+            })
+        })
+    })
+});
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+    res.send({
+        products: []
+    })
 })
 
 app.get('*', (req, res) => {
