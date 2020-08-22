21f61b4a3cdbbcf73eeea0fd12535a08f8d1979b
unknown <selenajosephine@gmail.com>
2020-08-23 00:00:45 +0530

    chat app changes for acknowledgement, location and filters

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,7 +1,41 @@
 const socket = io();
 
+// server emits -> client receives -> acknowledgment to server
+// client emits -> server receives -> acknowledgment to client
+
+// elements
+const $messageForm = document.querySelector('#message-form');
+const $messageFormInput = $messageForm.querySelector('input');
+const $messageFormButton = $messageForm.querySelector('button');
+const $locationButton = document.querySelector('#send-location');
+
 socket.on('message', (message) => console.log(message));
-document.querySelector('#messageform').addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    socket.emit('sendMessage', document.getElementById('message').value);
+    // disable  button
+    $messageFormButton.setAttribute('disabled', 'disabled');
+    socket.emit('sendMessage', $messageFormInput, (error) => {
+        // enable button and reset field
+        $messageFormButton.removeAttribute('disabled');
+        $messageFormInput.value = '';
+        $messageFormInput.focus();
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message Delivered');
+    });
+})
+
+$locationButton.addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported on your browser');
+    }
+    $locationButton.setAttribute('disabled', 'disabled');
+    navigator.geolocation.getCurrentPosition((position) => {
+        const { coords: { latitude, longitude } } = position;
+        socket.emit('sendLocation', { latitude, longitude }, () => {
+            $locationButton.removeAttribute('disabled');
+            console.log('Location shared!');
+        });
+    });
 })
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const socketio = require('socket.io');
+const Filter = require('bad-words');
 
 const app = express();
 // express library does it behind the scenes anyway. 
@@ -14,12 +15,26 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 // setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
-// let count = 0;
-
 io.on('connection', (socket) => {
     console.log('new web socket connection established');
     socket.emit('message', 'welcome');
-    socket.on('sendMessage', (message) => io.emit('message', message));
+    socket.broadcast.emit('message', 'New User has joined');
+    socket.on('sendMessage', (message, callback) => {
+        const filter = new Filter();
+        if (filter.isProfane(message)) {
+            return callback('Profanity is not allowed');
+        }
+
+        io.emit('message', message);
+        callback();
+    });
+    socket.on('sendLocation', ({ latitude, longitude }, callback) => {
+        socket.broadcast.emit('message', `https://google.com/maps?q=${latitude},${longitude}`);
+        callback();
+    });
+    socket.on('disconnect', () => {
+        io.emit('message', 'A use has left');
+    })
 });
 
 
